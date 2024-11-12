422af9bce54eadc848aa5be80298e095539c09ba
Marty Zhang <marty8zhang@gmail.com>
2024-11-13 10:50:03 +1100

    Complete solution 1 of "Greatest Common Divisor of Strings" in JavaScript.

---
+++ src/2_greatest-common-divisor-of-strings/js/solution-1.js
@@ -0,0 +1,32 @@
+﻿export function gcdOfStrings(str1, str2) {
+  const maxGcdLen = Math.min(str1.length, str2.length);
+  let gcd = '';
+  let cd = str1[0];
+
+  while (cd.length < maxGcdLen) {
+    if (
+      str1.replaceAll(cd, '').length === 0 &&
+      str2.replaceAll(cd, '').length === 0
+    )
+      gcd = cd;
+    cd = str1.substring(0, cd.length + 1);
+  }
+
+  /*
+   * In case `str1` is shorter, the length of `cd` won't be increased at the end
+   * of the `while` loop, then it won't be possible to terminate the loop with
+   * `cd.length <= maxGcdLen`. Hence, it'll be more efficient to test the full
+   * length of `str1` outside the loop for the last time. By this means, which
+   * string is shorter between `str1` and `str2` doesn't matter in this
+   * algorithm.
+   */
+  if (
+    str1.replaceAll(cd, '').length === 0 &&
+    str2.replaceAll(cd, '').length === 0
+  )
+    gcd = cd;
+
+  return gcd;
+}
+
+export default gcdOfStrings;
---
+++ src/2_greatest-common-divisor-of-strings/js/solution-1.test.js
@@ -0,0 +1,35 @@
+﻿import gcdOfStrings from './solution-1.js';
+
+describe('gcdOfStrings()', () => {
+  it('should return an empty string when there is no common divisor of the given strings', () => {
+    expect(gcdOfStrings('ABC', 'DEF')).toBe('');
+  });
+
+  it('should return an empty string when there is no common divisor of the given strings, despite their similarities', () => {
+    expect(gcdOfStrings('ABCDABC', 'ABC')).toBe('');
+  });
+
+  it('should return the expected result when the greatest common divisor is only one-char long', () => {
+    expect(gcdOfStrings('AAAAA', 'A')).toBe('A');
+  });
+
+  it('should return the expected result when the is only one char keeping repeating in both strings with uneven lengths between them', () => {
+    expect(gcdOfStrings('A'.repeat(7), 'A'.repeat(4))).toBe('A');
+  });
+
+  it('should return the expected result when there is only one char keeps repeating in both strings with even lengths between them', () => {
+    expect(gcdOfStrings('A'.repeat(8), 'A'.repeat(6))).toBe('AA');
+  });
+
+  it('should return the expected result when a substring keeps repeating in both strings with uneven lengths between them', () => {
+    expect(gcdOfStrings('ABC'.repeat(3), 'ABC'.repeat(2))).toBe('ABC');
+  });
+
+  it('should return the expected result when a substring keeps repeating in both strings with even lengths between them', () => {
+    expect(gcdOfStrings('ABC'.repeat(4), 'ABC'.repeat(2))).toBe('ABCABC');
+  });
+
+  it('should return the expected result when one string keeps repeating in another string', () => {
+    expect(gcdOfStrings('ABCDEFG'.repeat(3), 'ABCDEFG')).toBe('ABCDEFG');
+  });
+});

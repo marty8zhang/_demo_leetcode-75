e522e31946b477468838af8e525b40757d3b0c2c
Marty Zhang <marty8zhang@gmail.com>
2024-11-15 20:31:22 +1100

    Add `dev/initialise.ps1` to help with initialising .NET projects for each LeetCode 75 problem.
    Also,
    
    - Create `README.md` for this repo.
    - Move `README.md` of `4_can_place_flowers` to the correct location.

---
+++ README.md
@@ -0,0 +1,13 @@
+﻿# LeetCode 75
+
+This repository contains both JavaScript and C# solutions
+for [LeetCode 75](https://leetcode.com/studyplan/leetcode-75).
+
+# Helper Tools
+
+A PowerShell script has been added to help with initialising .NET projects for each LeetCode 75
+problem. E.g.,
+
+```powershell
+.\dev\initialise.ps1 1_merge-strings-alternately
+```
---
+++ dev/initialise.ps1
@@ -0,0 +1,39 @@
+﻿Write-Host -ForegroundColor Yellow -BackgroundColor Black "# Checking the folder structure..."
+
+if (!(Test-Path -Path ".\package.json"))
+{
+    throw "Error: Please run this script from the project root. E.g., ``.\dev\initialise.ps1 1_merge-strings-alternately``."
+}
+
+$problemSubDir = $args[0]
+if (!$problemSubDir)
+{
+    throw "Error: A subdirectory of a LeetCode problem should be provided."
+}
+
+$problemDir = ".\src\$( $problemSubDir )"
+if (!(Test-Path -Path "$( $problemDir )\README.md"))
+{
+    throw "Error: Could not find the problem `README.md`. Either create the file or check if the provided path name is correct or not."
+}
+
+$solutionDir = ".\src\$( $problemSubDir )\csharp\Program"
+$solutionTestsDir = "$( $solutionDir ).Tests"
+if ((Test-Path -Path $solutionDir) -or (Test-Path -Path $solutionTestsDir))
+{
+    throw "Error: The directory(ies) ``$( $solutionDir )`` and/or ``$( $solutionTestsDir )`` already exist(s)."
+}
+
+Write-Host -ForegroundColor Yellow -BackgroundColor Black "# Creating C# projects..."
+
+dotnet new console -o $solutionDir
+dotnet new xunit -o $solutionTestsDir
+dotnet add $solutionTestsDir reference $solutionDir
+
+Write-Host -ForegroundColor Yellow -BackgroundColor Black "# Adding unit testing dependencies..."
+
+Push-Location $solutionTestsDir
+dotnet add package FluentAssertions
+Pop-Location
+
+Write-Host -ForegroundColor Green -BackgroundColor Black "# Done!$( $PSStyle.Reset )"

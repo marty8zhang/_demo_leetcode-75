ca888db5cda760feb8ef9b97225ddf7c7f91b4a0
Marty Zhang <marty8zhang@gmail.com>
2024-12-17 16:15:10 +1100

    Complete solution 2 of "735. Asteroid Collision" in JavaScript.
    Also, improve the unit tests.

--- src/735_asteroid-collision/js/solutions.test.js
+++ src/735_asteroid-collision/js/solutions.test.js
@@ -1,49 +1,48 @@
-import asteroidCollision from './solution-1.js';
+// import asteroidCollision from './solution-1.js';
+import asteroidCollision from './solution-2.js';
 
 describe('`asteroidCollision()`', () => {
-  it('should return the positive bigger size asteroid, when there are only two asteroids in the given array', () => {
-    expect(asteroidCollision([100, -99])).toEqual([100]);
+  it('should return the same array, when there are only two asteroids and they move away from each other', () => {
+    expect(asteroidCollision([-1, 2])).toEqual([-1, 2]);
   });
 
-  it('should return the negative bigger size asteroid, when there are only two asteroids in the given array', () => {
+  it('should return the same array, when there are only two asteroids and they both move to the left', () => {
+    expect(asteroidCollision([-1, -2])).toEqual([-1, -2]);
+  });
+
+  it('should return the same array, when there are only two asteroids and they both move to the right', () => {
+    expect(asteroidCollision([1, 2])).toEqual([1, 2]);
+  });
+
+  it('should return the negative bigger size asteroid, when there are only two asteroids', () => {
     expect(asteroidCollision([100, -999])).toEqual([-999]);
   });
 
-  it('should return an empty array, when there are only two asteroids with the same size but different directions in the given array', () => {
+  it('should return the positive bigger size asteroid, when there are only two asteroids', () => {
+    expect(asteroidCollision([100, -99])).toEqual([100]);
+  });
+
+  it('should return an empty array, when there are only two asteroids with the same size and they move towards each other', () => {
     expect(asteroidCollision([1000, -1000])).toEqual([]);
-  });
-
-  it('should return the same array, when there are only two asteroids with negative values in the given array', () => {
-    expect(asteroidCollision([-1000, -10])).toEqual([-1000, -10]);
-  });
-
-  it('should return the same array, when there are only two asteroids with positive values in the given array', () => {
-    expect(asteroidCollision([10, 1000])).toEqual([10, 1000]);
   });
 
   it('should return the same array, when there are multiple asteroids and they never collide', () => {
     expect(asteroidCollision([-2, -1, 1, 2])).toEqual([-2, -1, 1, 2]);
   });
 
-  it('should return an empty array, when there are multiple asteroids in the given array, but all exploded at the end', () => {
-    expect(asteroidCollision([123, 98, 100, -123])).toEqual([]);
+  it('should return the expected result, when there are a series of collisions, and the last asteroid goes left', () => {
+    expect(asteroidCollision([-1, 2, -3])).toEqual([-1, -3]);
   });
 
-  it('should return the expected result, when there are multiple asteroids in the given array, and the last asteroid explodes', () => {
-    expect(asteroidCollision([5, 10, -5])).toEqual([5, 10]);
+  it('should return the expected result, when there are a series of collisions, and only the last asteroid is left', () => {
+    expect(asteroidCollision([122, 98, 100, -123])).toEqual([-123]);
   });
 
-  it('should return the expected result, when there are multiple asteroids in the given array, and only the last asteroid does not explode', () => {
-    expect(asteroidCollision([2, -1, -2, -2])).toEqual([-2]);
+  it('should return the expected result, when there are a series of collisions, and the last asteroid explodes', () => {
+    expect(asteroidCollision([5, 10, 3, -5])).toEqual([5, 10]);
   });
 
-  it('should return the expected result, when there are multiple asteroids in the given array, and the asteroids on the left end of the array do not explode', () => {
-    expect(asteroidCollision([-2, -2, 1, -1])).toEqual([-2, -2]);
-  });
-
-  it('should return the expected result, when there are multiple asteroids in the given array, and the asteroids on the right end of the array do not explode', () => {
-    expect(asteroidCollision([123, -98, 100, -1, -100, -1000, -7])).toEqual([
-      -1000, -7,
-    ]);
+  it('should return the expected result, when there are multiple a series of collisions, and the last asteroid explodes together with another one', () => {
+    expect(asteroidCollision([1, 4, 2, -4])).toEqual([1]);
   });
 });

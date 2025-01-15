96c6358a5064f163710c04554c05e1b8a7ebc3ec
Marty Zhang <marty8zhang@gmail.com>
2025-01-15 16:33:27 +1100

    Improve solution 1 of "328. Odd Even Linked List" in JavaScript according to the C# version.

--- src/328_odd-even-linked-list/js/solution-1.js
+++ src/328_odd-even-linked-list/js/solution-1.js
@@ -1,14 +1,14 @@
 export function oddEvenList(head) {
   const evenHead = head?.next;
-  if (evenHead === undefined || evenHead?.next === undefined) return head;
+  if (evenHead?.next === undefined) return head;
 
   let oddPointer = head;
   let evenPointer = evenHead;
   while (evenPointer?.next) {
-    oddPointer.next = oddPointer.next.next;
+    oddPointer.next = evenPointer.next;
     oddPointer = oddPointer.next;
     /*
-     * By removing `?? null` at the end can further improve performance, but
+     * Removing `?? null` at the end can further improve performance, but
      * `evenPointer.next` might end up with `undefined` rather than `null`.
      */
     evenPointer.next = evenPointer.next?.next ?? null;

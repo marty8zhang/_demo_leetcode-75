541890155eb07b0faacd408e45f3cc355194f04e
Marty Zhang <marty8zhang@gmail.com>
2025-01-17 18:06:43 +1100

    Complete solution 2 of "1161. Maximum Level Sum of a Binary Tree" in C#.

---
+++ src/1161_maximum-level-sum-of-a-binary-tree/csharp/Program/SolutionTwo.cs
@@ -0,0 +1,38 @@
+﻿using Utility;
+
+public class SolutionTwo
+{
+    public static int MaxLevelSum(TreeNode root)
+    {
+        var maxSum = root.val;
+        var maxSumLevel = 1;
+        var currentLevel = 1;
+        var queue = new Queue<TreeNode>();
+
+        queue.Enqueue(root);
+
+        while (queue.Count != 0)
+        {
+            var sum = 0;
+            var numberOfNodesOnTheCurrentLevel = queue.Count;
+            for (var i = 0; i < numberOfNodesOnTheCurrentLevel; i++)
+            {
+                var node = queue.Dequeue();
+                sum += node.val;
+
+                if (node.left != null) queue.Enqueue(node.left);
+                if (node.right != null) queue.Enqueue(node.right);
+            }
+
+            if (sum > maxSum)
+            {
+                maxSum = sum;
+                maxSumLevel = currentLevel;
+            }
+
+            currentLevel++;
+        }
+
+        return maxSumLevel;
+    }
+}
